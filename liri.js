04455391e609ac9a8c7318482dc471dda5a15ab5
Your Name <hautran1993@yahoo.com>
2017-11-30 16:32:02 -0600

    toched up

--- liri.js
+++ liri.js
@@ -2,11 +2,7 @@
 var keys = require("./keys.js");
 var fs = require("fs");
 var input1 = process.argv[2];
-// var name = input2.join("+");
-// var cmd = name.split("")
 var input2 = process.argv[3];
-console.log('outside', input2)
-//needs to make search available for using multiple words by using .split and .join
 
 //function to tell liri what to do
 function grabCmd(str){
@@ -51,8 +47,6 @@ function grabStr(str){
     }
   })
 }
-//0 as the path
-//1 has the file name. thats why you are always starting with 2
 //function for grabing tweets
 function grabTweet(){
   const Twitter = require("twitter");
@@ -88,14 +82,14 @@ function grabSong(song){
       client.search({
         type: 'track',
         query: song,
-        limit: 1
+        limit: 3
       },
       function(err, data) {
           if (err) {
             console.log('Error occurred: ' + err);
             return;
           }
-
+          //shorten 
           const ope = data.tracks.items[0]
           var str = "";
           str += "Song: " + ope.name + '\n'
@@ -120,9 +114,8 @@ function grabMovie(movie){
     if(!error && response.statusCode === 200) {
       var json = JSON.parse(body)
       console.log(json);
+      // getting ready to log every thing stored
       var str = "";
-      //The forEach() method executes a provided function once for each array element.
-      //think about using it for rotten tomato.
       str += "Tittle: " + json.Title + '\n'
       str += "released Year : " + json.Year + '\n'
       str += "IMDB Rating : " + json.imdbRating + '\n'
@@ -137,19 +130,6 @@ function grabMovie(movie){
       console.log(str);
 
     }
-    // if(json.Ratings[1].Value === true) {
-    //   str += "Rotten Tomatoes Rating : " + json.Ratings[1].Value + '\n'
-    // }
   })
 
 };
-
-// need to make constant song and movie to show if nothing is entered
-//make double words possible ex: hey you
-//finish grabMovie
-//finish appending logs
-//finish json.package
-//create a empty variable name str
-//push all files in to the str
-//consolelog the starting
-//use log function to log in str into log.txt
